07fe7693dc6f8dd29806791a45d1f16ac5ac9f6f
urban III shocker <urban.shocker@mailbox.org>
2022-09-20 22:26:26 -0500

    Update to Assignment 6

--- Assignment6/Assignment6/Assignment6.cpp
+++ Assignment6/Assignment6/Assignment6.cpp
@@ -1,20 +1,141 @@
-// Assignment6.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+
+using namespace std;
+
+#include "Results.h"
+
+void getdata(int arr [],const char*, int);
+bool ordSearch(int list[], int size, int target, int& compares);
+bool unOrdSearch(int list[], int size, int target, int& compares);
+bool binSearch(int list[], int size, int target, int& compares);
+void updateStats(Results&);
+void displayResults(const Results&, const Results&, const Results&);
+void process(int list[], int search[], int, Results& res, bool (*func)(int list[], int size, int target, int &compares));
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int ordered[1000];
+    int unordered[1000];
+    int searchNums[500];
+    Results unord;
+    Results ord;
+    Results bin;
+
+    getdata(ordered, "ordered.txt", 1000);
+    getdata(unordered, "unordered.txt", 1000);
+    getdata(searchNums, "searchNums.txt", 500);
+
+    process(ordered, searchNums, 1000, ord, ordSearch);
+    process(unordered, searchNums, 1000, unord, unOrdSearch);
+    //process(ordered, searchNums, 1000, bin, binSearch);
+
+    updateStats(ord);
+    updateStats(unord);
+    //updateStats(bin);
+
+    displayResults(ord, unord, bin);
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void getdata(int arr[],const char* fname, int arrsize)
+{
+
+    ifstream infile(fname);
+
+    int num;
+    int i = 0;
+    while(i < arrsize && infile >> num)
+    {
+        arr[i++] = num;
+    }
+
+}
+
+bool ordSearch(int list[], int size, int target, int& compares)
+{
+    bool found = false;
+    int i = 0;
+    while (i < size && list[i] < target)
+    {
+        i++;
+        compares++;
+    }
+    if (i < size && list[i] == target)
+    {
+        found = true;
+        compares++;
+    }
+
+    return found;
+}
+
+bool unOrdSearch(int list[], int size, int target, int& compares)
+{
+    bool found = false;
+    int i = 0;
+    while (i < size && list[i] != target)
+    {
+        i++;
+        compares++;
+    }
+    if (i < size && list[i] == target)
+    {
+        found = true;
+        compares++;
+    }
+
+    return found;
+}
+
+bool binSearch(int list[], int size, int target, int& compares)
+{
+    
+
+    return false;
+}
+
+void updateStats(Results& res)
+{
+
+    res.percSuccesses = ((res.numSuccesses / 500.0) * 100);
+    res.avgNumCompares = res.totalCompares / 500.0;
+
+}
+
+void displayResults(const Results& resOrd, const Results& resUnOrd, const Results& resBin)
+{
+
+    printf("%40s%10s%10s\n", "Unordered", "Ordered", "Bin");
+    printf("Number of Successful Searches %10i%10i%10i\n\n", resUnOrd.numSuccesses, resOrd.numSuccesses, resBin.numSuccesses);
+    printf("Precent of Successful Searches %9.2f%%%9.2f%%%9.2f%%\n\n", resUnOrd.percSuccesses, resOrd.percSuccesses, resBin.percSuccesses);
+    printf("Average compares per search %12.0f%10.0f%10.0f\n\n", resUnOrd.avgNumCompares, resOrd.avgNumCompares, resBin.avgNumCompares);
+
+}
+
+void process(int list[], int search[],int size, Results& res, bool(*func)(int list[], int size, int target, int &compares))
+{
+    int numcomp;
+    bool found;
+
+    for (int i = 0; i < 500; i++)
+    {
+        numcomp = 0;
+        found = func(list, size, search[i], numcomp);
+
+        
+        if (found)
+        {
+            
+            res.numSuccesses++;
+        }   
+
+        res.totalCompares += numcomp;
+
+    }
+
+    
+
+}
--- Assignment6/Assignment6/Results.h
+++ Assignment6/Assignment6/Results.h
@@ -1,20 +1,22 @@
 #ifndef RESULTS_H
 #define RESULTS_H
 
+
+
 struct Results
 {
 	int numSuccesses,
 		totalCompares;
 	double percSuccesses,
 		avgNumCompares;
+
+	Results();
 };
 
 Results::Results()
 {
 	numSuccesses = 0;
 	totalCompares = 0;
-	percSuccesses = 0.0;
-	avgNumCompares = 0.0;
 }
 
 

a59e98dbd4ac9519b73a9acb4b7d68b3ba806bb9
urban III shocker <urban.shocker@mailbox.org>
2022-09-22 17:43:21 -0500

    Final update assignment 6

--- Assignment6/Assignment6/Results.h
+++ Assignment6/Assignment6/Results.h
@@ -1,7 +1,29 @@
+//*****************************************************************************************************
+//
+//		File:					Results.h
+//
+//		Student:				Urban Shocker
+//
+//		Assignment:				Program #06
+//
+//		Course Name:			Data Structures II
+//
+//		Course Number:			COSC 3100-01
+//
+//		Due:					22/09/22
+//
+//
+//		This program is an additional file for searches.cpp
+//	
+//		Other files required: 
+//			1.	Searches.cpp
+//			
+//*****************************************************************************************************
+
 #ifndef RESULTS_H
 #define RESULTS_H
 
-
+//*****************************************************************************************************
 
 struct Results
 {
@@ -12,6 +34,8 @@ struct Results
 
 	Results();
 };
+
+//*****************************************************************************************************
 
 Results::Results()
 {
